4e8d79fa51c3bf16393749c7c6c2a3f42f46d361
dev-diegov <diegoidv.us@gmail.com>
2023-09-12 20:52:59 -0300

    tasks details, views and template

--- src/base/urls.py
+++ src/base/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import PendingsList
+from .views import PendingsList, TaskDetail
 
-urlpatterns = [path('', PendingsList.as_view(), name="pendientes")]
+urlpatterns = [path('', PendingsList.as_view(), name="pendings"),
+               path('tasks/<int:pk>', TaskDetail.as_view(), name="tasks")]
 
