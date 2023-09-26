32f0516075bca6365ce02e0e5da2c5ece7eeffed
dev-diegov <diegoidv.us@gmail.com>
2023-09-25 23:00:05 -0300

    login & logout

--- src/base/urls.py
+++ src/base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import PendingsList, TaskDetail, CreateTask, EditTask, DeleteTask
+from django.contrib.auth.views import LogoutView
+from .views import PendingsList, TaskDetail, CreateTask, EditTask, DeleteTask, Login
 
 urlpatterns = [path('', PendingsList.as_view(), name="tasks"),
+               path('login/', Login.as_view(), name="login"),
+               path('logout/', LogoutView.as_view(next_page='login'), name="logout"),
                path('tasks/<int:pk>', TaskDetail.as_view(), name="task"),
                path('create-task/', CreateTask.as_view(), name="create-task"),
                path('edit-task/<int:pk>', EditTask.as_view(), name="edit-task"),
--- src/base/views.py
+++ src/base/views.py
@@ -2,17 +2,30 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Task
 
-class PendingsList(ListView):
+class Login(LoginView):
+    '''
+    Login logic
+    '''
+    template_name = "base/login.html"
+    field = "__all__"
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+class PendingsList(LoginRequiredMixin, ListView):
     '''
     Show a list ob tasks (objects)
     '''
     model = Task
     context_object_name = 'tasks'
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     '''
     Show one task detail
     '''
@@ -20,7 +33,7 @@ class TaskDetail(DetailView):
     context_object_name = 'task'
     template_name = "base/task_detail.html"
 
-class CreateTask(CreateView):
+class CreateTask(LoginRequiredMixin, CreateView):
     '''
     Create new tasks
     '''
@@ -28,7 +41,7 @@ class CreateTask(CreateView):
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-class EditTask(UpdateView):
+class EditTask(LoginRequiredMixin, UpdateView):
     '''
     Edit a task
     '''
@@ -36,7 +49,7 @@ class EditTask(UpdateView):
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-class DeleteTask(DeleteView):
+class DeleteTask(LoginRequiredMixin, DeleteView):
     '''
     Delete a task
     '''
