44b562096d1b5f917b6e320746fd419ff4460963
dev-diegov <diegoidv.us@gmail.com>
2023-09-25 22:22:49 -0300

    new task model and template

--- src/base/urls.py
+++ src/base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PendingsList, TaskDetail
+from .views import PendingsList, TaskDetail, CreateTask
 
 urlpatterns = [path('', PendingsList.as_view(), name="pendings"),
-               path('tasks/<int:pk>', TaskDetail.as_view(), name="tasks")]
+               path('tasks/<int:pk>', TaskDetail.as_view(), name="tasks"),
+               path('create-task/', CreateTask.as_view(), name="create-task")]
 
--- src/base/views.py
+++ src/base/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Task
-
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Task
 
 class PendingsList(ListView):
+    '''
+    Show a list ob tasks (objects)
+    '''
     model = Task
-    context_object_name = "tasks"
+    context_object_name = 'tasks'
 
 class TaskDetail(DetailView):
+    '''
+    Show one task detail
+    '''
     model = Task
-    context_object_name = "task"
+    context_object_name = 'task'
     template_name = "base/task_detail.html"
+
+class CreateTask(CreateView):
+    '''
+    Create new tasks
+    '''
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
