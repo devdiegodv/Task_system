8027ae8a52c8f16028c1ed2639a6820895c3f782
dev-diegov <diegoidv.us@gmail.com>
2023-09-25 22:30:35 -0300

    add task

--- src/base/urls.py
+++ src/base/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import PendingsList, TaskDetail, CreateTask
 
-urlpatterns = [path('', PendingsList.as_view(), name="pendings"),
-               path('tasks/<int:pk>', TaskDetail.as_view(), name="tasks"),
+urlpatterns = [path('', PendingsList.as_view(), name="tasks"),
+               path('tasks/<int:pk>', TaskDetail.as_view(), name="task"),
                path('create-task/', CreateTask.as_view(), name="create-task")]
 
